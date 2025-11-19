8a01958154a9394abfc544e964e5744adc2c6b31
Jesika Kotaeva <DKotaeva@inno.tech>
2025-11-19 18:19:47 +0300

    изменена логика

--- src/main/java/com/stepup/proj/Main.java
+++ src/main/java/com/stepup/proj/Main.java
@@ -8,7 +8,7 @@ public class Main {
         System.out.println("----7");
     }
 
-    public static void main(String[] args) {
+    public static void doSomething () {
         System.out.println("Hello World!");
     }
 }
